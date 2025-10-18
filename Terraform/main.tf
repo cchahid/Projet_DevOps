fc741e10b9309c1425cc3336b89141cb36d73e6d
cchahid <c.chahidachraf@gmail.com>
2025-10-18 13:25:03 +0100

    Initial commit of DevOps project

---
+++ Terraform/main.tf
@@ -0,0 +1,155 @@
+# Create Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup"
+  location = "West Europe"
+
+}
+
+# Create Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myVNet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Create Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "mySubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create Network Security Group (NSG)
+resource "azurerm_network_security_group" "nsg" {
+  name                = "myNSG"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Add NSG Rule for SSH
+resource "azurerm_network_security_rule" "ssh" {
+  name                        = "AllowSSH"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "0.0.0.0/0"  # Allow from any IP (or restrict to your IP)
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+# Associate NSG with Subnet
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Create Public IP for Master Node
+resource "azurerm_public_ip" "master_ip" {
+  name                = "master-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# Create Public IP for Worker Node 1
+resource "azurerm_public_ip" "worker1_ip" {
+  name                = "worker1-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+
+
+# Create Network Interface for Master Node
+resource "azurerm_network_interface" "master_nic" {
+  name                = "master-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.master_ip.id
+  }
+}
+
+# Create Network Interface for Worker Node 1
+resource "azurerm_network_interface" "worker1_nic" {
+  name                = "worker1-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.worker1_ip.id
+  }
+}
+
+
+
+# Create Master VM
+resource "azurerm_linux_virtual_machine" "master_vm" {
+  name                  = "masterVM"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_B2ms"
+  admin_username        = "azureuser"
+  network_interface_ids = [azurerm_network_interface.master_nic.id]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("/home/khalil/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
+# Create Worker VM 1
+resource "azurerm_linux_virtual_machine" "worker1_vm" {
+  name                  = "worker1VM"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_B2s"
+  admin_username        = "azureuser"
+  network_interface_ids = [azurerm_network_interface.worker1_nic.id]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("/home/khalil/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
